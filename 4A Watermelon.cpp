d8b3b0fcb304c760e10a247f2533efe26e543d2d
Hamza Yehia <86762463+HamzaYehia@users.noreply.github.com>
2021-10-04 13:40:29 +0200

    Add files via upload

---
+++ 4A Watermelon.cpp
@@ -0,0 +1,18 @@
+// problem name: 4A Watermelon
+// problem link: https://codeforces.com/problemset/problem/4/A
+// status: accepted
+
+// auother: https://github.com/HamzaYehia
+
+
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int w = 0;
+    cin >> w;
+    string output = (w <= 2 || w % 2) ? "NO" : "YES";
+    cout << output << endl;
+    return 0;
+}
