ff13c1740ebf9b51a2a4023d530ef3382b5b80db
Hamza Yehia <86762463+HamzaYehia@users.noreply.github.com>
2021-10-04 13:44:33 +0200

    Add files via upload

---
+++ 71A Way Too Long Words.cpp
@@ -0,0 +1,24 @@
+// problem name: 71A Way Too Long Words
+// problem link: https://codeforces.com/contest/71/problem/A
+// status: accepted
+
+// auother: https://github.com/HamzaYehia
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    string word;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> word;
+        if (word.size() > 10)
+            cout << word[0] << word.length() - 2 << word[word.length() - 1] << endl;
+        else
+            cout << word << endl;
+    }
+}
