af99d8a83d890182571aadbeb6102c3e7d76024f
Hamza Yehia <86762463+HamzaYehia@users.noreply.github.com>
2021-12-17 15:47:33 +0200

    Add files via upload

---
+++ A. Stones on the Table.c
@@ -0,0 +1,27 @@
+// problem name: Stones on the Table
+// problem link: https://codeforces.com/problemset/problem/266/A
+// status: accepted
+ 
+// auother: https://github.com/HamzaYehia
+// CodeForces Acc: https://codeforces.com/profile/HamzaYehia 
+
+#include<stdio.h>
+
+int main(void)
+{
+    int n;
+    scanf("%d\n", &n);
+    char arr[n];
+    scanf("%s", arr);
+    int counter = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+	if (arr[i] == arr[i + 1])
+	{
+	    counter++;
+	}
+    }
+    printf("%d", counter);
+	   
+}
+
