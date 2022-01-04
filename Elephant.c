647e72ff34f3b39578649981080d22393d470e2e
Hamza Yehia <86762463+HamzaYehia@users.noreply.github.com>
2022-01-04 23:15:23 +0200

    Add files via upload

---
+++ Elephant.c
@@ -0,0 +1,24 @@
+// problem name: Elephant
+// problem link: https://codeforces.com/problemset/problem/617/A
+// status: accepted
+
+// auother: https://github.com/HamzaYehia
+// CodeForces Acc: https://codeforces.com/profile/HamzaYehia
+
+
+#include<stdio.h>
+
+int main(void)
+{
+    int distance, steps;
+    scanf("%d", &distance);
+
+    if (distance % 5 == 0) {
+        steps = (distance / 5);
+    }
+    else {
+        steps = (distance / 5) + 1;
+    }
+    printf("%d", steps);
+    return 0;
+}
