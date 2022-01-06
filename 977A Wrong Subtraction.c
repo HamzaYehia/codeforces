77e51a98934720e62112af6f12707263e02f9e5d
Hamza Yehia <86762463+HamzaYehia@users.noreply.github.com>
2022-01-07 00:15:25 +0200

    Add files via upload

---
+++ 977A Wrong Subtraction.c
@@ -0,0 +1,32 @@
+// problem name: Wrong Subtraction
+// problem link: https://codeforces.com/problemset/problem/977/A
+// status: accepted
+
+// auother: https://github.com/HamzaYehia
+// CodeForces Acc: https://codeforces.com/profile/HamzaYehia
+
+
+#include<stdio.h>
+
+int main(void)
+{
+    int n, k;
+    scanf("%d %d", &n, &k);
+    for (int i = 0; i < k; i++)
+    {
+        /* (n % 10) figure out
+        the value of the last digit of n */
+        
+        if ((n % 10) != 0)
+        {
+            n -= 1;
+        }
+        else
+        {
+            n /= 10;
+        }
+    }
+
+    printf("%d", n);
+    return 0;
+}
