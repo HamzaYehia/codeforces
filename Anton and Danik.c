bd6b623359a68142ed698f2b120ef09663036439
Hamza Yehia <hamzayehiamakhlouf@gmail.com>
2022-03-17 22:34:25 +0200

    accepted

---
+++ Anton and Danik.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int Anton = 0, Danik = 0, n = 0;
+	
+	scanf("%d", &n);
+	char  S[n + 1];
+	
+	scanf("%s", S); 
+	
+	for (int i = 0; i < sizeof(S) - 1; i++) {
+		
+		if (S[i] == 'A') {
+			Anton += 1;
+			}
+			
+		else if (S[i] == 'D') {
+			Danik += 1;
+			}
+	}
+	
+	
+	if (Anton > Danik) {
+		printf("Anton\n");
+	}
+	else if (Danik > Anton) {
+		printf("Danik\n");
+	}
+	else {
+		printf("Friendship\n");
+	}
+	
+	return 0;
+}
