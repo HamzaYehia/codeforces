1db72e1ada7ce1b18bf5b5d101ed571fcd2c553d
Hamza Yehia <hamzayehiamakhlouf@gmail.com>
2022-03-17 22:43:09 +0200

    accepted

--- Anton and Danik.c
+++ Anton and Danik.c
@@ -1,26 +1,34 @@
+/* problem name: Anton and Danik
+ * problem link: https://codeforces.com/problemset/problem/734/A
+ *
+ * time complexity: Linear time O(n)
+ * auother: https://github.com/HamzaYehia
+ * CodeForces Acc: https://codeforces.com/profile/HamzaYehia
+ */
+
+
 #include <stdio.h>
 
-int main(void)
-{
+int main(void) {
 	int Anton = 0, Danik = 0, n = 0;
-	
+
 	scanf("%d", &n);
 	char  S[n + 1];
-	
-	scanf("%s", S); 
-	
+
+	scanf("%s", S);
+
 	for (int i = 0; i < sizeof(S) - 1; i++) {
-		
+
 		if (S[i] == 'A') {
 			Anton += 1;
-			}
-			
+		}
+
 		else if (S[i] == 'D') {
 			Danik += 1;
-			}
+		}
 	}
-	
-	
+
+
 	if (Anton > Danik) {
 		printf("Anton\n");
 	}
@@ -30,6 +38,6 @@ int main(void)
 	else {
 		printf("Friendship\n");
 	}
-	
+
 	return 0;
 }
